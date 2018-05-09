ec16109936ad2413b18c314d37d9f865709f0089
Adam-Collier <adamcollierart@gmail.com>
2018-05-09 08:39:18 +0100

    nav, searchbar and remove markdown anchors

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "Midl",
+  description: "test",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Interviews", link: "/interviews/" },
+      { text: "Mixes", link: "/mixes/" }
+    ]
+  },
+  markdown: {
+    anchor: {
+      permalink: false,
+      permalinkBefore: false,
+      permalinkSymbol: ""
+    }
+  }
+};
