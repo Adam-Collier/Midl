e50fbfae0e2443ddc676994554e20a1a450df0f6
Adam Collier <adamcollierart@gmail.com>
2018-10-11 15:30:50 +0100

    interviews available as prototype

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,7 +2,32 @@ import mixes from './mixes.js'
 
 export default ({
     Vue, // the version of Vue being used in the VuePress app
+    options, // the options for the root Vue instance
+    router, // the router instance for the app
+    siteData // site metadata
 }) => {
     // ...apply enhancements to the app
     Vue.prototype.$mixes = mixes;
+
+    let interviews = siteData.pages
+        .filter(x => {
+            console.log;
+            return x.path.match(/(?=.*interviews)(?=.*html)/);
+        })
+        .sort((a, b) => {
+            return (
+                b.frontmatter.date
+                    .split(".")
+                    .reverse()
+                    .join("") -
+                a.frontmatter.date
+                    .split(".")
+                    .reverse()
+                    .join("")
+            );
+        });
+    interviews[0].first = true;
+
+    Vue.prototype.$interviews = interviews;
+
 }
